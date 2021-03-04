01ee9164ff97e2dad58d966c83c1620f53d870bb
Leandro Kieliger <leandro.kieliger@swisscom.com>
2021-03-04 17:51:49 +0100

    Print agent action in demo

--- src/demo.py
+++ src/demo.py
@@ -7,6 +7,21 @@ from environments import utils as env_utils
 
 from helpers import cli
 
+ORANGE = '\033[1m\x1B[31m'
+NOCOLOR = '\033[0m'
+
+
+def colored_button(button_str, value):
+    if value > 0:
+        return f'{ORANGE}{button_str}: {value}{NOCOLOR}'
+    else:
+        return f'{button_str}: {value}'
+
+
+def print_action(buttons, possible_actions, action_index):
+    print(' | '.join([colored_button(b, a) for b, a in zip(buttons, possible_actions[action_index])]), end=' \r', flush=True)
+
+
 if __name__ == '__main__':
     # Extract command line arguments
     parser = cli.get_parser()
@@ -25,13 +40,20 @@ if __name__ == '__main__':
     # Load agent
     agent = conf.get_agent(env=env, load_from=load_from)
 
+    # Extract button acronyms
+    buttons = env.venv.envs[0].game.get_available_buttons()
+    buttons = [str(b).split('.')[1] for b in buttons]
+
+    # Extract possible actions
+    possible_actions = env.venv.envs[0].possible_actions
+
     for i in range(10):
         obs = env.reset()
         done = False
-        images = []
         while not done:
             action, _ = agent.predict(obs, deterministic=False)
+            print_action(buttons, possible_actions, action[0])
             obs, reward, done, _ = env.step(action)
-            time.sleep(1/35.0)
+            time.sleep(1/45.0)
 
     env.close()
